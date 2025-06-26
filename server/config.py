42711d0a12dd6231109f17c495b99f423ae1cbb1
Mohamed-Abass2287 <aminabass987@gmail.com>
2025-06-26 21:03:27 +0300

    Configured Flask app with SQLAlchemy, Migrate

---
+++ server/config.py
@@ -0,0 +1,7 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_restful import Api
+from flask_cors import CORS
+from sqlalchemy import MetaData
+from flask_jwt_extended import JWTManager
