d537e40bf2031033b0c1b8da8384dee30fe5dc85
kennedy218218 <kennedy.mwai@student.moringaschool.com>
2025-06-26 22:39:54 +0300

    added demo user and set a password

--- server/seed.py
+++ server/seed.py
@@ -5,6 +5,11 @@ with app.app_context():
     db.drop_all()
     db.create_all()
 
+    user = User(username='demo')
+    user.set_password('password')
+    db.session.add(user)
+    db.session.commit()
+
 
 
 
