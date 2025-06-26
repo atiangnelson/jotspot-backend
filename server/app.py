a08f8a2c9d7ce825570ab420fb6ec402f84a80f7
Timothy <timothy.waweru1@student.moringaschool.com>
2025-06-26 23:14:14 +0300

    created class login and rgister

--- server/app.py
+++ server/app.py
@@ -3,3 +3,22 @@ from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identi
 from config import app, db, api
 from models import User, Note
 from flask_restful import Resource
+class Register(Resource):
+    def post(self):
+        data = request.get_json()
+        if User.query.filter_by(username=data['username']).first():
+            return {'message': 'Username already exists'}, 400
+        user = User(username=data['username'])
+        user.set_password(data['password'])
+        db.session.add(user)
+        db.session.commit()
+        return {'message': 'User created'}, 201
+
+class Login(Resource):
+    def post(self):
+        data = request.get_json()
+        user = User.query.filter_by(username=data['username']).first()
+        if user and user.check_password(data['password']):
+            token = create_access_token(identity=user.id)
+            return {'access_token': token}, 200
+        return {'message': 'Invalid credentials'}, 401
