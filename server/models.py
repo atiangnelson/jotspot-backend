e79a80c8a358ba5af9802d0cc184b3aedc3ec1ca
Nelson <nelson.atiang@student.moringaschool.com>
2025-06-27 16:42:28 +0300

    Added password verification method and serialization rules to User model

--- server/models.py
+++ server/models.py
@@ -14,5 +14,10 @@ class User(db.Model,SerializerMixin):
     def set_password(self,password):
         self.password_hash=generate_password_hash(password)
 
+    def check_password(self,password):
+        return check_password_hash(self.password_hash,password)
+
+    serialize_rules = ('-password_hash','-notes.user',)        
+
         
 
