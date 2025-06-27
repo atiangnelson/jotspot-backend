38f1d3e8c8f96ea1eba65dcaaa39e6f09f3b326b
Nelson <nelson.atiang@student.moringaschool.com>
2025-06-27 08:17:06 +0300

    Added User model with username, hashed password, and notes relationship

--- server/models.py
+++ server/models.py
@@ -1,3 +1,13 @@
 from config import db
 from sqlalchemy_serializer import SerializerMixin
 from werkzeug.security import generate_password_hash,check_password_hash
+
+class User(db.Model,SerializerMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer,primary_key=True)
+    username=db.Column(db.String,unique=True,nullable=False)
+    password_hash=db.Column(db.String,nullable=False)
+
+    notes=db.relationship('Note', backref='user',lazy=True)
+
